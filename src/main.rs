cb60774230620222497fe2255eedf30e9c5eff6b
Irreplaceable <1349694984@qq.com>
2026-03-18 18:17:29 +0800

    更换运行时，改为monoio, 修改main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,36 @@
 use anyhow::Result;
-use std::sync::Arc;
+use monoio::net::TcpListener;
+use monoio::RuntimeBuilder;
 use time::format_description;
-use tokio::net::TcpListener;
-use tokio::time::{Duration, interval};
-
-use mini_redis::context::Context;
-use mini_redis::db::Db;
-use mini_redis::handler::handle_connection;
-use mini_redis::pubsub::PubSub;
 use time::macros::offset;
-use tracing::Instrument;
-use tracing::{info, info_span};
+use tracing::info;
 use tracing_subscriber::fmt;
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<()> {
-    // redis-benchmark -h 127.0.0.1 -p 6377 -c 50 -n 100000 -t get,set -P 16
-    // redis基准测试 pipeline模式 get qps:719424/s set qps:354609/s (windows 11 24h2数据)
-    // pipeline模式 get qps:1449275/s set qps:970873/s (wsl2 ubuntu 24.04)
-    // redis-benchmark -h 127.0.0.1 -p 6377 -c 50 -n 100000 -t set,get
-    // 普通模式 get qps:66934/s  set qps: 67069/s(windows 11 24h2数据)
-    // 普通模式 get qps:103305/s  set qps: 100200/s (wsl2 ubuntu 24.04)
-    init_log().await;
-    let listener = TcpListener::bind(("0.0.0.0", 6377)).await?;
-    info!("mini redis listening on {:?}", listener.local_addr()?);
 
-    let ctx = Arc::new(Context::new(Db::new(), PubSub::new()));
-
-    // 启动定期清理过期 key 的后台任务
-    let cleanup_ctx = ctx.clone();
-    tokio::spawn(async move {
-        tokio::time::sleep(Duration::from_secs(5 * 60)).await;
-        let mut intv = interval(Duration::from_secs(30));
-        loop {
-            intv.tick().await;
-            cleanup_ctx.db().clean_up();
-        }
-    });
-
-    loop {
-        let (socket, addr) = listener.accept().await?;
-        socket.set_nodelay(true)?;
-        let span = info_span!("handle_connection", client_addr = %addr);
-        let conn_ctx = ctx.clone();
-        tokio::spawn(
-            async move {
-                if let Err(e) = handle_connection(socket, conn_ctx).await {
-                    info!("connection error: {}", e);
+fn main() -> Result<()> {
+    init_log();
+    RuntimeBuilder::<monoio::FusionDriver>::new()
+        .with_entries(32768)
+        .enable_timer()
+        .build()?
+        .block_on(async {
+            let tcp_listener = TcpListener::bind("0.0.0.0:6377").unwrap();
+            info!("Mini Redis Listening on: {}", tcp_listener.local_addr().unwrap());
+            loop {
+                let in_coming = tcp_listener.accept().await;
+                match in_coming {
+                    Ok((socket, addr)) => {
+                        println!("accepted a connection from {}", addr);
+                    }
+                    Err(err) => {}
                 }
             }
-            .instrument(span),
-        );
-    }
+        });
+
+   Ok(())
 }
 
-async fn init_log() {
+fn init_log() {
     let timer_format = format_description::parse(
         "[year]-[month padding:zero]-[day padding:zero] [hour]:[minute]:[second]",
     )
